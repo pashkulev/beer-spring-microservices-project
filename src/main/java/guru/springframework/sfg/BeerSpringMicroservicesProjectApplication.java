e8ca91c9097d8efcdaee469dd5bfb164acec1acc
Ivan Pashkulev <ivan.pashkulev@gmail.com>
2022-08-17 18:55:09 +0300

    Initial commit

---
+++ src/main/java/guru/springframework/sfg/BeerSpringMicroservicesProjectApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerSpringMicroservicesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerSpringMicroservicesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfg/BeerSpringMicroservicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerSpringMicroservicesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
